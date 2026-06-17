54d8c07269e9809d9f75130c90c4a9a4d8e53e0f
Usman Aslam <usmaanaslam04@gmail.com>
2026-06-17 18:42:32 +0500

    told ya

--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,9 @@ import Navbar from "@/Components/Navbar";
 import TopBanner from "@/Components/TopBanner";
 import HeroSection from "@/Components/HeroSection";
 import AboutSection from "@/Components/AboutSection";
-import ServicesSection from "@/Components/ServicesSection";
-import TechSection from "@/Components/TechStackSection";
-import ProjectsSection from "@/Components/ProjectsSection";
+import ShowcaseSection from "@/Components/ShowcaseSection";
 import FooterSection from "@/Components/FooterSection";
+
 
 const Home = () => {
   return (
@@ -22,11 +21,10 @@ const Home = () => {
       </div>
 
       <div id="services">
-        <ServicesSection />
+        <ShowcaseSection />
       </div>
 
-      <TechSection />
-      <ProjectsSection />
+      
 
       <div id="contact">
         <FooterSection />
